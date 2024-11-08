91bf8e2f2c06432a2af909eb653d279736c4dcbd
raylann_lopes <raylanlopes@icloud.com>
2024-11-08 15:59:11 -0300

    feat: add authentication with Clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,11 @@ import { SignInButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       {/* Esquerda */}
@@ -23,10 +27,12 @@ const LoginPage = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       {/* Direita */}
       <div className="relative h-full w-full">
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const home = () => {
-  return <Button>Click me</Button>;
+const home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return (
+    <div className="h-full flex itens-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default home;
