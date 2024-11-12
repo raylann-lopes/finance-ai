28703de13bab1565265aae75ef7ce320dc292274
raylann_lopes <raylanlopes@icloud.com>
2024-11-12 20:42:29 -0300

    feat: ensure pages are correctly scrolled by the user

--- app/(home)/_components/percentage-item.tsx
+++ app/(home)/_components/percentage-item.tsx
@@ -9,11 +9,12 @@ interface PercentageItemProps {
 const PercentageItem = ({ icon, title, value }: PercentageItemProps) => {
   return (
     <div className="flex justify-between items-center">
-      <div className="flex items-center gap-2">
-        {icon}
+      <div className="flex items-center gap-3">
+        <div className="rounded-lg bg-white p-2 bg-opacity-[3%]">{icon}</div>
+
         <p className="text-sm text-muted-foreground">{title}</p>
       </div>
-      <p className="font-bold text-sm">{value}</p>
+      <p className="font-bold text-sm">{value}%</p>
     </div>
   );
 };
