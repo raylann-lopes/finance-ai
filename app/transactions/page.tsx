fdc7e7a12f39abc63f18ad0f45aef08fef28cc3b
raylann_lopes <raylanlopes@icloud.com>
2024-11-18 01:08:23 -0300

    refactor: ensure decima warning does not happen

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -32,7 +32,10 @@ const TransactionsPage = async () => {
           <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
         <ScrollArea>
-          <DataTable columns={transactionColumns} data={transactions} />
+          <DataTable
+            columns={transactionColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
         </ScrollArea>
       </div>
     </>
